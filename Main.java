5368df6dfe0dc3b5652edd61c6ece75e6a130c0a
Jarmo Hurri <jarmo.hurri@iki.fi>
2020-11-08 06:49:01 +0200

    * change main class name to Main

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-public class VendingMachineApp
+public class Main
 {
   public static void main (String[] args)
   {
